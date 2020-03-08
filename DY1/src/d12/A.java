613ffe51bef26327d80183dfa40ef15547105db9
priya-000 <Triptimankar08@gmail.com>
2020-03-08 11:45:01 +0530

    asdfy

---
+++ DY1/src/d12/A.java
@@ -0,0 +1,23 @@
+package d12;
+
+import Dy2.main;
+
+public class A {
+	
+		A(){
+			System.out.println(" in constructor");
+		}
+		
+		static {
+			
+			System.out.println("in static block");
+		}
+		
+		{
+		
+		
+		System.out.println(" printing non  static block");
+		
+	}
+
+}
