fa38b4f8a0dd30966234af8c38b385e00de1c232
priya-000 <Triptimankar08@gmail.com>
2020-03-08 11:47:14 +0530

    aa

---
+++ DY1/src/d14/address.java
@@ -0,0 +1,12 @@
+package d14;
+
+public class address {
+	int pincode;
+	
+	String landmark;
+	
+ /*	@Override
+	public String toString() {
+		return "address [pincode=" + pincode + ", landmark=" + landmark + "]";
+	} */
+}
